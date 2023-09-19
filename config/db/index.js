e4ffbd959d24ab0ad15e21f7d8baf0c21f951f4d
Dang Huu Trung <trungdangdev06@gmail.com>
2023-09-19 21:09:23 +0700

    Update index.js

--- config/db/index.js
+++ config/db/index.js
@@ -2,7 +2,7 @@ import { mongoose } from "mongoose";
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/Trunghd_Blog");
+    await mongoose.connect("mongodb://127.0.0.1:27017/Trunghd_Blog");
     console.log("Connect successfully!!");
   } catch (error) {
     console.log("Connect failure!");
